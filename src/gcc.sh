30db465c65a9039ac4b5ddb3f5bcd2aa32a26fcb
Mrhouse <127476912+mrhouse-sweet@users.noreply.github.com>
2024-10-18 13:55:06 +0800

    Update gcc.sh

--- src/gcc.sh
+++ src/gcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CPLUS_INCLUDE_PATH=/home/ti5robot/mechanical_arm_7_axis/include:$CPLUS_INCLUDE_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ti5robot/mechanical_arm_7_axis/include/can
-g++ main.cpp communication.cpp mathfunc.cpp Ti5BASIC.cpp  Ti5MOVE.cpp Ti5LOGIC.cpp tool.cpp     electrical_machinery.cpp  -L../include/can  -I/usr/include/python3.10 -lpython3.10 -lmylibscan -lcontrolcan -lspdlog -lfmt -ludev  -o move_sov
+export CPLUS_INCLUDE_PATH=~/LeftArmMotionSolver/include:$CPLUS_INCLUDE_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/LeftArmMotionSolver/include/can
+g++ main.cpp  -L./include -lmylibti5 -L./include/can -I/usr/include/python3.10 -lpython3.10 -lmylibscan -lcontrolcan -lspdlog -lfmt -ludev -o move_sov
